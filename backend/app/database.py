3cca96ede4726eb41443da940c23d7734b9153d1
Ross Paton <rossp2258@gmail.com>
2026-05-19 18:40:37 +0100

    set up docker and database config

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,28 @@
-"""
-handles connection between app and database
-"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+import os
+
+# reads .env files
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL) # creates connection
+SessionLocal = sessionmaker(bind=engine) # each request gets its own session
+
+
+# used to track tables
+# all models will inherit this
+class Base(DeclarativeBase):
+    pass
+
+# opens session and hands it to route
+def get_db():
+
+    db = SessionLocal()
+    try:
+        yield db
+
+    finally:
+        db.close()
