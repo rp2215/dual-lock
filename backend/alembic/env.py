1d61c3af5656c2af5502b9f5746ed9e0088aeeec
Ross Paton <rossp2258@gmail.com>
2026-05-22 20:17:34 +0100

    updated env script

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.database import Base, DATABASE_URL
-from backend.app.models import vault_entries
+from app.models import user, vault, entry
 
 target_metadata = Base.metadata
 
